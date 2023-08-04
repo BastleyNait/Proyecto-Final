c0d17bce09bef5e1c704823406e25ae741c0e5ee
BastleyNait <schirinosne@unsa.edu.pe>
2023-08-04 17:08:24 -0500

    Solucionando error de no renderizado

--- tiendaOnline/urls.py
+++ tiendaOnline/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tienda import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -26,5 +25,3 @@ urlpatterns = [
     path('', views.home, name='home')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
