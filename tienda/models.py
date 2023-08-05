369d0a6fc81abb2e4745525c32d5cc4e0e40b1e7
BastleyNait <schirinosne@unsa.edu.pe>
2023-08-05 00:00:00 -0500

    cambios extranios

--- tienda/models.py
+++ tienda/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    id_categoria = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+    image_url = models.URLField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"nombre: {self.name} categoria: {self.category}"
 
 class Customer(models.Model):
     name = models.CharField(max_length=200)
